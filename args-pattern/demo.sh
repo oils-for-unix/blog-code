39afa401c4e190ae3fe7a9208085b863efa2cf2c
Andy C <andy@chubot.org>
2017-01-16 23:11:51 -0800

    Demo code for next post

---
+++ args-pattern/demo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Usage:
+#   ./demo.sh <function name>
+
+set -o nounset
+set -o pipefail
+set -o errexit
+
+parallel-each() {
+  xargs -P 4 -n 1 -- $0 "$@"
+}
+
+# Some expensive thing
+sleep-hello() {
+  local seconds=$1
+  echo "hello for $seconds"
+  sleep $seconds
+}
+
+peach-demo() {
+  time echo '0.1 0.2 0.3' | parallel-each sleep-hello
+}
+
+"$@"
