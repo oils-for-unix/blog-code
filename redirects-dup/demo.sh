26f51e99d366d424c83612c3cff748c4a9ab1a8e
Andy C <andy@chubot.org>
2017-02-09 14:37:00 -0800

    Adding more stuff

--- redirects-dup/demo.sh
+++ redirects-dup/demo.sh
@@ -48,4 +48,43 @@ EOF
 # ./stdout_stderr.py !2 > !1  >/dev/null | tr A-Z a-z > $out
 # ./stdout_stderr.py !2 > !1  !1 > /dev/null | tr A-Z a-z > $out
 
+# Maybe:
+#
+# ./stdout_stderr.py !2 | tr A-Z a-z > $out
+# 
+# But then what happens to stdout?
+
+# http://stackoverflow.com/questions/2342826/how-to-pipe-stderr-and-not-stdout
+
+pipe-stderr-keep-stdout() {
+  mkdir -p _tmp/
+  local out=_tmp/stderr.txt
+
+  # must dup stderr to stdout first, and then stdout to /dev/null
+  ./stdout_stderr.py 3>&1 1>&2 2>&3 | tr A-Z a-z > $out
+
+  compare $out - <<EOF
+stderr
+EOF
+}
+
+process-subs() {
+  mkdir -p _tmp/
+  local out=_tmp/stdout.txt
+  local err=_tmp/stderr.txt
+
+  # must dup stderr to stdout first, and then stdout to /dev/null
+  ./stdout_stderr.py > >(tr A-Z a-z > $out) 2> >(tr A-Z a-z > $err)
+
+  compare $out - <<EOF
+stdout
+EOF
+  compare $err - <<EOF
+stderr
+EOF
+}
+
+# oil:
+# ./stdout_stderr.py !1 > $>[tr A-Z a-z > $out] !2 > $>[tr A-Z a-z > $err]
+
 "$@"
