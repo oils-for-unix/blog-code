3ec754a3ee8d7c1e6fe9bedd4fc8621af7b32b79
Andy Chu <andy@oilshell.org>
2017-08-20 20:52:48 -0700

    Experimenting with -MD flag

--- dotd/run.sh
+++ dotd/run.sh
@@ -73,4 +73,40 @@ demo-short() {
   demo -f short.mk
 }
 
+show-flag() {
+  local flag=$1
+  rm -f main.d
+  # -M writes to stdout
+  echo "... $flag ..."
+  gcc $flag main.c
+  echo
+}
+
+flags() {
+  show-flag -M
+  test -f main.d && { echo "Should not exist"; exit 1; }
+
+  # -MD is equivalent to -M -MF file, except that -E is not implied.  -E means
+  show-flag -MD
+  show-file main.d
+
+  show-flag -MMD
+  show-file main.d
+}
+
+# Invoke the preprocessor directly
+preprocessor-deps() {
+  # How to write it to a file, but not preprocess?
+  cpp main.c -M -MF my_custom_name.d
+  show-file my_custom_name.d
+
+  # Doesn't generate .o file
+  gcc -c main.c -M -MF my_custom_name2.d
+  show-file my_custom_name2.d
+  #ls -l main.o
+
+  # TODO: I don't think there's way to compile and generate .d to a custom name?  Two outputs?
+}
+
+
 "$@"
