ca02b0eaa785717cfe5cb680e95c8e6ac209c086
Andy Chu <andy@lisa>
2017-09-27 01:23:58 -0700

    Solution that is safe against adversarial input.
    Use NUL bytes as delimiters, and also grep for NUL beforehand.

--- git-changelog/demo-multiline.sh
+++ git-changelog/demo-multiline.sh
@@ -14,10 +14,22 @@ escape-segments() {
 import cgi, re, sys
 
 print re.sub(
-  r"\x01([^\x02]*)\x02", 
+  r"\x00([^\x00]*)\x00", 
   lambda match: cgi.escape(match.group(1)),
   sys.stdin.read())
 '
+}
+
+# nul check with od.  Point-free style.
+check-nul() {
+  # -A n: no addresses
+  # -t x1: 1 byte hex addresses
+  od -A n -t x1 | grep 00 
+}
+
+test-check-nul() {
+  python -c "print '\0'" | check-nul && echo YES
+  python -c "print '\1'" | check-nul || echo NO
 }
 
 git-log-html() {
@@ -25,24 +37,31 @@ git-log-html() {
 
   # - a trick for HTML escaping (avoid XSS): surround %s with unlikely bytes,
   #   0x01 and 0x02.  Then pipe Python to escape.
-  local format=$'
+  local format='
   <tr>
     <td> <a href="https://github.com/oilshell/blog-code/commit/%H">%h</a> </td>
     <td> %ad </td>
-    <td> \x01%an\x02 </td>
+    <td> %x00%an%x00 </td>
   </tr>
   <tr>
     <td></td>
-    <td colspan=2><pre>\x01%B\x02</pre></td>
+    <td colspan=2><pre>%x00%B%x00</pre></td>
   </tr>
   '
-  git log -n 5 --pretty="format:$format" | escape-segments
+
+  local plain='%H %ad %an %B'
+  if git log --pretty="format:$plain" "$@" | check-nul; then
+    echo 1>&2 "FATAL: git log contains NUL characters"
+    return 1
+  fi
+  git log "$@" --pretty="format:$format" | escape-segments
 
   echo '</table>'
 }
 
 write-file() {
-  git-log-html > git-log-multiline.html
+  # NOTE: with -n 5 there is a slight race condition.
+  git-log-html -n 5 > git-log-multiline.html
   echo "Wrote git-log-multiline.html"
 }
 
