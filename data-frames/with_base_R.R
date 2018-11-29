78975ca202fb834eed325334a6d9133998fbb84f
Andy Chu <andy@oilshell.org>
2018-11-29 11:16:36 -0800

    change examples

--- data-frames/with_base_R.R
+++ data-frames/with_base_R.R
@@ -16,34 +16,19 @@ main = function(argv) {
   Log('Loaded data:')
   print(traffic)
 
+  daily = aggregate(traffic$num_hits, by=list(date=traffic$date), FUN=sum)
+  Log('Daily traffic:')
+  print(daily)
+
   total_hits = sum(traffic$num_hits)
   Log('Total traffic to /blog/ = %d', total_hits)
 
   percentage = function(num_hits) {
     sum(num_hits) / total_hits * 100.0
   }
-
-  summary = aggregate(traffic$num_hits, by=list(url=traffic$url), FUN=percentage)
-
-  Log('Summary by URL:')
-  print(summary)
-
-  return()
-
-  traffic = data.frame(
-     date = as.Date(c('2018-11-30', '2018-11-30', '2018-12-01', '2018-12-01')),
-     url = c('/releases.html', '/blog/', '/releases.html', '/blog/'),
-     hits = c(42, 1000, 84, 2000)
-  )
-  print(traffic)
-  cat('\n')
-
-  # Filter rows of the data frame.
-  blog = subset(traffic, url == '/blog/')
-  print(blog)
-  cat('\n')
-
-  Log('Total traffic to /blog/ = %d', sum(blog$hits))
+  popular = aggregate(traffic$num_hits, by=list(url=traffic$url), FUN=percentage)
+  Log('Popular Pages:')
+  print(popular)
 }
 
 main(commandArgs(TRUE))
--- data-frames/with_dplyr.R
+++ data-frames/with_dplyr.R
@@ -19,9 +19,6 @@ main = function(argv) {
   Log('Loaded data:')
   print(traffic)
 
-  total_hits = sum(traffic$num_hits)
-  Log('Total traffic to /blog/ = %d', total_hits)
-
   traffic %>%
     group_by(date) %>%
     summarize(num_hits = sum(num_hits)) ->
@@ -29,12 +26,14 @@ main = function(argv) {
   Log('Daily traffic:')
   print(daily)
 
+  total_hits = sum(traffic$num_hits)
+  Log('Total traffic to /blog/ = %d', total_hits)
+
   traffic %>%
     group_by(url) %>%
     summarize(percentage = sum(num_hits) / total_hits * 100.0) %>%
     arrange(desc(percentage)) ->
     popular # assigned to variable summary
-
   Log('Popular Pages:')
   print(popular)
 }
