2b422ba4b62505dd873c68f0fd667fd6a763752f
Andy Chu <andy@oilshell.org>
2018-11-29 11:21:05 -0800

    Update pandas version

--- data-frames/with_pandas.py
+++ data-frames/with_pandas.py
@@ -7,15 +7,28 @@ import sys
 import pandas as pd
 
 
+def log(msg, *args):
+  print()
+  if args:
+    msg = msg % args
+  print('  ' + msg)
+
+
 def main(argv):
   traffic = pd.read_csv('traffic.csv')
+  log('Loaded:')
   print(traffic)
 
   total_hits = sum(traffic.num_hits)
-  print(total_hits)
+  log('Total hits = %d', total_hits)
 
-  summary = traffic.groupby('url').apply(lambda x: sum(x.num_hits) / total_hits * 100.0)
-  print(summary)
+  daily = traffic.groupby('date').apply(lambda x: sum(x.num_hits))
+  log('Daily Traffic:')
+  print(daily)
+
+  popular = traffic.groupby('url').apply(lambda x: sum(x.num_hits) / total_hits * 100.0)
+  log('Popular Pages:')
+  print(popular)
 
 
 if __name__ == '__main__':
