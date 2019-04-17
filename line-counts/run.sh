a1c934edd68f6bb8048bfb94aeb0733b08b3cfda
Andy Chu <andy@oilshell.org>
2019-04-17 00:09:33 -0700

    Include .def files in the bash line counts.

--- line-counts/run.sh
+++ line-counts/run.sh
@@ -10,11 +10,14 @@ set -o errexit
 readonly BASH_DIR=~/src/languages/bash-4.4
 
 bash-files() {
+  # NOTE: .def files are builtins
+
   find $BASH_DIR \
     \( -name 'y.tab.c' -a -prune \) -o \
     \( -name 'readline' -a -prune \) -o \
     \( -name parse.y -a -print \) -o \
-    \( -name '*.[ch]' -a -print \)
+    \( -name '*.[ch]' -a -print \) -o \
+    \( -name '*.def' -a -print \)
 }
 
 readline-files() {
@@ -23,18 +26,20 @@ readline-files() {
 }
 
 significant() {
-  # 87K sloccount
-  # 88K cloc
+  # 87K sloccount (I think this excluded .def files)
+  # 101K cloc
   bash-files | xargs cloc
+  #bash-files | xargs sloccount
   echo
 
   # 24K sloccount and cloc
   readline-files | xargs cloc
+  #readline-files | xargs sloccount
   echo
 }
 
 physical() {
-  # 124K raw lines
+  # 142K raw lines
   bash-files | xargs wc -l | sort -n | tail -n 20
   echo
 
