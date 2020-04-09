2fb6acff1ae935e31e59b9147ede2850e5e6584f
Andy Chu <andy@oilshell.org>
2020-04-09 01:46:51 -0700

    ok got a variant of prog3 to work

--- pipe-coroutine/sieve.sh
+++ pipe-coroutine/sieve.sh
@@ -9,6 +9,11 @@ set -o nounset
 set -o pipefail
 set -o errexit
 
+download() {
+  wget http://www.cs.dartmouth.edu/~doug/sieve/sieve.bash
+  wget http://www.cs.dartmouth.edu/~doug/sieve/sieve.c
+}
+
 src () {
   seq 2 100
 }
@@ -20,14 +25,44 @@ cull() {
   done
 }
 
-sink() {
+sink2() {
   read p
   echo $p
-  cull $p | sink &
+
+  # Ah this is recursive!
+  cull $p | sink2 &
 }
 
-main() {
-  src | sink
+prog2() {
+  src | sink2
+}
+
+#
+# Program 3
+#
+
+die() {
+  echo "$@" 1>&2
+  exit 1
+}
+
+sink3() {
+  read -u $primes pp
+  while
+    read p
+    # Hm this fixes a bug?
+    test -n "$p" || die "done"
+    (($p < $pp * $pp))
+  do
+    echo $p
+  done
+  cull $pp | sink3 &
+}
+
+prog3() {
+  rm -f fifo
+  mkfifo fifo
+  (echo 2; (src | sink3 {primes}<fifo)) | tee fifo
 }
 
 "$@"
