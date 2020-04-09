e31f4780f904c58c350019628241a25ba9297139
Andy Chu <andy@oilshell.org>
2020-04-09 02:16:34 -0700

    ok trying more

--- pipe-coroutine/sieve.sh
+++ pipe-coroutine/sieve.sh
@@ -10,12 +10,15 @@ set -o pipefail
 set -o errexit
 
 download() {
-  wget http://www.cs.dartmouth.edu/~doug/sieve/sieve.bash
-  wget http://www.cs.dartmouth.edu/~doug/sieve/sieve.c
+  wget --no-clobber \
+    http://www.cs.dartmouth.edu/~doug/sieve/sieve.bash \
+    http://www.cs.dartmouth.edu/~doug/sieve/sieve.c \
+    https://golang.org/doc/play/sieve.go
 }
 
-src () {
-  seq 2 100
+src() {
+  local end=${1:-100}
+  seq 2 $end
 }
 
 cull() {
@@ -62,8 +65,70 @@ sink3() {
 prog3() {
   rm -f fifo
   mkfifo fifo
-  # Why is there an extra 2 here?  Curious.
+  # Why is there an extra 2 here?  Weird.
   { echo 2; src | sink3 {primes}<fifo; } | tee fifo
 }
 
+#
+# Is a Go port possible?
+# We need a way to make a pipe.
+#
+
+filter() {
+  local in=$1
+  local out=$2
+  local prime=$3
+
+  while read i; do
+    if (( i % prime != 0 )); then
+      echo $i >& $out
+    fi
+  done <& $in
+}
+
+test-filter() {
+  seq 20 | filter 0 1 3
+}
+
+go-port() {
+  # ah crap, fd is 10 in another process!  Not in this shell
+  # we can't access it
+  #src 10 {fd}>&1  &  # go generate(ch)
+
+  local n=10
+
+  # We need a way do express this
+  # Really it should be 
+
+  # Well anything you can do with pipe(), you can do with mkfifo?
+
+  # pipe :r :w
+  # src $n >& $w
+  for (( i = 0; i < 10; i++ )); do
+    read -u $r :prime
+    echo $prime
+    pipe :r2 :w2
+
+    filter $r $w2 $prime
+
+    r=r2
+    #w=r2
+  done
+
+  src $n &
+
+  for i in $(seq $n); do
+    #echo $i
+    :
+  done
+
+  wait
+  echo 'done'
+}
+
+
+
 "$@"
+
+
+
