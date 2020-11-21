324905e47812bbfe1ba6a74a02378a29d1d74da7
Andy Chu <andy@oilshell.org>
2020-11-21 13:24:08 -0800

    [data-frames] Got the window function working.
    But I don't understand the OVER () thing?

--- data-frames/run.sh
+++ data-frames/run.sh
@@ -112,36 +112,35 @@ EOF
 
 # Window functions use the "OVER" keyword.
 # https://sqlite.org/windowfunctions.html
+# Only available as of version 3.25 as of 9/2018!
 
-# Doesn't work?  Syntax error on line 1.  sqlite supports the WINDOW keyword
-# for intermediate definitions.
+sqlite3-new() {
+  ~/src/sqlite-autoconf-3330000/sqlite3 "$@"
+}
+
+# From peter on lobste.rs:
+# https://lobste.rs/s/0mpcxv/tab_programming_language#c_ewkmc6
+
+# This is cool, but I don't really get why you need OVER ()
 
 with-window() {
-  sqlite3 $DB <<EOF
-SELECT date, url, num_hits FROM traffic;
+  # The subtotals query
+  sqlite3-new $DB <<EOF
+  SELECT url, sum(num_hits) AS url_hits
+  FROM traffic
+  GROUP BY url
 EOF
-
-sqlite3 <<EOF
-CREATE TABLE t0(x INTEGER PRIMARY KEY, y TEXT);
-INSERT INTO t0 VALUES (1, 'aaa'), (2, 'ccc'), (3, 'bbb');
-SELECT x, y, row_number() OVER (ORDER BY y) AS row_number FROM t0 ORDER BY x;
-EOF
-  return
 
   echo
-  sqlite3 $DB <<EOF
-SELECT url, sum(num_hits) OVER (GROUP BY url) as hits_per_url FROM traffic;
-EOF
-  return
 
-  sqlite3 $DB <<EOF
-WITH total_hits_per_url AS (
-  SELECT url, sum(hits) AS hits
+  sqlite3-new $DB <<EOF
+WITH subtotals AS (
+  SELECT url, sum(num_hits) AS url_hits
   FROM traffic
   GROUP BY url
 )
-SELECT url, hits * 100.0 / sum(hits) OVER () AS percentage
-FROM total_hits_per_url
+SELECT url, url_hits * 100.0 / sum(url_hits) OVER () AS percentage
+FROM subtotals
 ORDER BY percentage desc;
 EOF
 }
