9bfe3ec516c6cfbae5ae4c2e7a087ae99c72b18e
andy-macbook <andy@oilshell.org>
2025-01-31 20:03:03 -0500

    Another demo

---
+++ bash-bugs/melvin-demo.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "BASH_VERSION = $BASH_VERSION"
+
+set -o errexit
+
+foo() {
+        set +o errexit
+        if command time -f true > /dev/null;
+        then
+                echo 'inside'
+        fi
+        set -o errexit
+        echo 'outside'
+}
+
+foo &
+
+echo waiting
+wait
+echo done
+
