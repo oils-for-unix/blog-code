328f01f1d72f128c0388940aeced31b135393c45
Andy C <andy@oilshell.org>
2025-03-03 02:14:29 -0500

    Demo of Terraform-like config with Hay

---
+++ hay/run.sh
@@ -0,0 +1,6 @@
+
+# Run YSH from the repo, because this demo relies on a recent bug fix
+# As of 2025, Hay is still in development, and can use feedback/testing
+
+~/git/oils-for-unix/oils/bin/ysh iac-demo.ysh > output.txt
+
