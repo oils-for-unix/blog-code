3947b6c0170fcf1d361573c1d9e62237494698ee
Andy C <andy@oils.pub>
2025-08-15 14:28:28 -0400

    [cgroups] Demo of using cgroups without root, using systemd-run
    It apparently sends a DBus message, so it doesn't need root.

--- cgroups/run.sh
+++ cgroups/run.sh
@@ -22,7 +22,7 @@ fork-recklessly() {
   echo '<<< fork'
 }
 
-term-kill-cgroup() {
+shell-term-kill() {
   # create a new control group
 
   local group=/sys/fs/cgroup/cgroup-demo
@@ -48,5 +48,32 @@ term-kill-cgroup() {
 
   echo '<<< demo'
 }
+
+rootless-with-systemd() {
+  systemd-run --user --scope --unit my-demo -- $0 fork-recklessly &
+  sleep 0.2
+
+  systemctl --user list-units --type=scope
+  sleep 0.2
+
+  # Kill as user
+  systemctl --user kill --signal=TERM my-demo.scope
+  sleep 0.2
+
+  #systemctl --user kill --signal=KILL my-demo.scope
+}
+
+# Why does this work without root?  Claude says it uses DBus to the systemd daemon:
+
+# # systemd-run sends a message like this over D-Bus:
+# busctl --user call \
+#   org.freedesktop.systemd1 \
+#   /org/freedesktop/systemd1 \
+#   org.freedesktop.systemd1.Manager \
+#   StartTransientUnit \
+#   "ssa(sv)a(sa(sv))" \
+#   "my-scope.scope" "replace" 2 \
+#   "PIDs" "au" 1 $$ \
+#   "Slice" "s" "user.slice"
 
 "$@"
